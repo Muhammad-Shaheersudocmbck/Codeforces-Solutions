f81c453ae024f95c9c17f75115c6490dc1cf4c2d
Muhammad-Shaheersudocmbck <shaheermain573@gmail.com>
2026-04-20 07:02:07 +0500

    Add CF 148-356933796 A

---
+++ Codeforces/148/A_Insomnia_cure_356933796.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+ans = 0
+for i in range(1,e+1):
+  if i%a==0 or i%b==0 or i%c==0 or i%d==0:
+    ans+=1
+print(ans)
