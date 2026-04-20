10a495159418e6d165fc0d35e49ecbd7022385a1
Muhammad-Shaheersudocmbck <shaheermain573@gmail.com>
2026-04-20 06:56:16 +0500

    Add CF 2218-369618734 A

---
+++ Codeforces/2218/A_The_67th_Integer_Problem_369618734.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int long long
+#define pb push_back
+const int MOD = 1e9+7;
+
+void solve(){
+  
+    int a;
+    cin>>a;
+    cout<<67<<"\n";
+
+}
+
+signed main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    long long tc = 1;
+    cin>>tc;
+    while (tc--){
+        solve();
+    }
+    return 0;
+}
