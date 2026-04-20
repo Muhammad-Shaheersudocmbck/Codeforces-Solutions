1e004c051cef103367a9a3857b05a11cc98301ae
Muhammad-Shaheersudocmbck <shaheermain573@gmail.com>
2026-04-20 06:54:54 +0500

    Add CF 1855-371449571 B

---
+++ Codeforces/1855/B_Longest_Divisors_Interval_371449571.cpp
@@ -0,0 +1,53 @@
+/*
+
+███████╗██╗  ██╗ █████╗ ██╗  ██╗███████╗███████╗██████╗ 
+██╔════╝██║  ██║██╔══██╗██║  ██║██╔════╝██╔════╝██╔══██╗
+███████╗███████║███████║███████║█████╗  █████╗  ██████╔╝
+╚════██║██╔══██║██╔══██║██╔══██║██╔══╝  ██╔══╝  ██╔══██╗
+███████║██║  ██║██║  ██║██║  ██║███████╗███████╗██║  ██║
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝
+                                                                   
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("O3","unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define pb push_back
+#define ll long long
+#define int ll
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define print(a) for(auto &i:a){cout << i << ' ';} cout << endl;
+
+void solve(){
+    
+    int a;
+    cin>>a;
+    int div = 0;
+    
+        for (int i = 1; i <= a; ++i)
+           {
+               if(a%i==0){
+                div++;
+               }
+               else{
+                break;
+               }
+           }   
+    cout<<div<<endl;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin>>tc;
+    while(tc--){
+        solve();
+    }
+}
