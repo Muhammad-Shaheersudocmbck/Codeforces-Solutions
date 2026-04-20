a6f0ff2043e5e048336dc0f7d82b335cda73d8e1
Muhammad-Shaheersudocmbck <shaheermain573@gmail.com>
2026-04-20 06:55:58 +0500

    Add CF 1352-370057034 C

---
+++ Codeforces/1352/C_K_th_Not_Divisible_by_n_370057034.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int long long
+#define pb push_back
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("O3","unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+bool goodboy(int n,int mid, int k){
+    int OK= (((int)(mid/n))*(n-1))+(mid-(((int)(mid/n))*n));
+    if (OK>= k && (mid%n)!=0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int idk(int n, int k){
+    int l = 1;int r = 1e18;
+    int ans = 0;
+    while(l<r){
+        int mid = l+(r-l)/2;
+        if (mid-mid/n == k and mid%n != 0) {
+            ans = mid;
+            break;
+        } else if (mid-mid/n < k) {
+            l = mid+1;
+        } else {
+            r = mid;
+        }
+    }
+    return ans;
+}
+
+
+void solve() {
+
+	int a,b;
+    cin>>a>>b;
+    cout<<idk(a,b)<<"\n";
+    
+}
+signed main(){
+
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    long long tcs = 1;
+    cin >> tcs;
+    while (tcs--){
+        solve();
+    }
+    return 0;
+}
