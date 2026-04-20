485ebd2ae874da51985dcdc163ddac5a9078ef05
Muhammad-Shaheersudocmbck <shaheermain573@gmail.com>
2026-04-20 06:54:51 +0500

    Add CF 1475-371454938 A

---
+++ Codeforces/1475/A_Odd_Divisor_371454938.cpp
@@ -0,0 +1,58 @@
+/*
+
+███████╗██╗  ██╗ █████╗ ██╗  ██╗███████╗███████╗██████╗ 
+██╔════╝██║  ██║██╔══██╗██║  ██║██╔════╝██╔════╝██╔══██╗
+███████╗███████║███████║███████║█████╗  █████╗  ██████╔╝
+╚════██║██╔══██║██╔══██║██╔══██║██╔══╝  ██╔══╝  ██╔══██╗
+███████║██║  ██║██║  ██║██║  ██║███████╗███████╗██║  ██║
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝
+                                                                   
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize("O3","unroll-loops")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define pb push_back
+#define ll long long
+#define int ll
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define print(a) for(auto &i:a){cout << i << ' ';} cout << endl;
+
+void solve(){
+    
+    int a;
+    cin>>a;
+    bool found = false;
+    if(a%2!=0){
+        cout<<"YES"<<endl;
+    }else{
+        while(a!=1){
+            a = a/2;
+            if(a%2!=0 and a!=1){
+                cout<<"YES"<<endl;
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc = 1;
+    cin>>tc;
+    while(tc--){
+        solve();
+    }
+}
